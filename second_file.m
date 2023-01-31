8e8161026689348effdb6ebfa0e808a35610e0f5
jishnu <goswamijishnume@gmail.com>
2023-01-31 12:29:27 +0530

    updated second_file
    steps to be followed while making changes locally and reflect it in the remote repository

---
+++ second_file.m
@@ -0,0 +1,5 @@
+This is a new file created to see the following things  - 
+1. To make changes to this file by adding some text. 
+2. Now make these visible in the remote git repository too
+
+Practice these steps a few times to become good at it 
